d8cfd19b89c62b23dad6b0864f8f4e675fcc3923
chuan92 <hzhchuan@gmail.com>
2013-07-13 09:28:58 +0000

    add database code

---
+++ database/DataInsert.sql
@@ -0,0 +1,7 @@
+load data local infile "MajorInfo" into table MajorInfo;
+load data local infile "UserInfo" into table UserInfo;
+load data local infile "TeacherInfo" into table TeacherInfo;
+load data local infile "StudentInfo" into table StudentInfo;
+load data local infile "CourseInfo" into table CourseInfo;
+load data local infile "CourseSelection" into table CourseSelection;
+
